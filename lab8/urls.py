98656e5ec96e894733dec829e882fac687cc666b
Luisa Mejia <luisa.mejia@uisek.edu.ec>
2026-06-09 20:55:19 -0500

    laboratorio7 terminado

--- lab8/urls.py
+++ lab8/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pokedex.urls')),
+    path('api/', include('api.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
